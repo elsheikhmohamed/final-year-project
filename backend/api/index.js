ba40ed0544d0e67d417631f952a19ec1de188300
mohamed <elsheikhmoha@gmail.com>
2023-04-04 16:18:15 +0100

    login completed

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,9 +10,16 @@ import cors from 'cors';
 
 
 //middleware routes
-
-app.use(express.json())
-app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
--- backend/client/src/pages/login/Login.jsx
+++ backend/client/src/pages/login/Login.jsx
@@ -1,13 +1,30 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import "./login.scss";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   const { login } = useContext(AuthContext);
 
-  const handleLogin = () => {
-    login();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/");
+    } catch (err) {
+      setErr(err.response.data);
+    }
   };
 
   return (
@@ -15,8 +32,21 @@ const Login = () => {
       <div className="login-card">
         <h1>Login</h1>
         <form className="login-form">
-          <input type="text" id="username" placeholder="Username" />
-          <input type="password" id="password" placeholder="Password" />
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            name="username"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
+          {err && err}
 
           <button className="login-btn" onClick={handleLogin}>
             Login
